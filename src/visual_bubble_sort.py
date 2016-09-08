bb985d57c71ff12b966eaf8ff59bda559970d7aa
heray1990 <heray1990@hotmail.com>
2016-09-08 16:47:24 +0800

    Add animation in visual_bubble_sort.py. For now, move first bar from left right. And highlight the moving bar during the movement.

--- src/visual_bubble_sort.py
+++ src/visual_bubble_sort.py
@@ -1,14 +1,29 @@
+"""
+Visualizing Bubble Sorting.
+"""
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-plt.subplots()
+fig, ax = plt.subplots()
 
 xpos = np.arange(0,100)
-mylist = range(1,101)
-random.shuffle(mylist)
-ypos = np.asarray(mylist)
+datalist = range(1,101)
+random.shuffle(datalist)
+ypos = np.asarray(datalist)
 
-plt.bar(xpos, ypos, alpha=0.4)
+rects = ax.bar(xpos, ypos, alpha=0.4)
+
+def animate(i):
+    if i > 0:
+        tmp = rects[i - 1].get_height()
+        rects[i - 1].set_height(rects[i].get_height())
+        rects[i].set_height(tmp)
+        rects[i - 1].set_color('b')
+        rects[i].set_color('y')
+    return ax,
+
+ani = animation.FuncAnimation(fig, animate, 100, repeat=False, blit=True)
 
 plt.show()
