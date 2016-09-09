0b5575870ac01c08d440077f476c267113149f2a
heray1990 <heray1990@hotmail.com>
2016-09-09 14:15:18 +0800

    Set the length of array in command line. Save animation into a gif file. Add init() and "__main__" function.

--- src/visual_bubble_sort.py
+++ src/visual_bubble_sort.py
@@ -1,20 +1,21 @@
 """
 Visualizing Bubble Sorting.
 """
+import sys
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-length = 10 
-fig, ax = plt.subplots()
+def init():
+    global length, rects, fig, ax
+    fig, ax = plt.subplots()
 
-xpos = np.arange(0, length)
-datalist = range(1, length + 1)
-random.shuffle(datalist)
-ypos = np.asarray(datalist)
-
-rects = ax.bar(xpos, ypos, alpha=0.4)
+    xpos = np.arange(0, length)
+    datalist = range(1, length + 1)
+    random.shuffle(datalist)
+    ypos = np.asarray(datalist)
+    rects = ax.bar(xpos, ypos, alpha=0.4)
 
 def index_gen():
     for i in range(0, (length - 1)):
@@ -33,6 +34,15 @@ def animate(i):
 
     return rects
 
-ani = animation.FuncAnimation(fig, animate, index_gen, repeat=False, blit=True)
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        length = int(sys.argv[1])
+    else:
+        length = 20
 
-plt.show()
+    init()
+    ani = animation.FuncAnimation(fig, animate, index_gen, repeat=False, blit=True)
+    if len(sys.argv) > 2 and sys.argv[2] == 'save':
+        ani.save('animation.gif', writer='imagemagick', fps=3)
+    else:
+        plt.show()
