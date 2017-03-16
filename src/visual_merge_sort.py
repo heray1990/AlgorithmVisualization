9579071bf91af377a4b6d257d9e9d0cf0ecee32a
heray1990 <heray1990@hotmail.com>
2017-03-16 17:22:42 +0800

    Update merge_sort() function. There is NOT any animation now.

---
+++ src/visual_merge_sort.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+"""
+Visualizing Merge Sort.
+"""
+import sys, getopt
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def init_animate():
+    i = 0
+    for rect in rects:
+        rect.set_height(ypos[i])
+        i += 1
+
+def merge(A, p, q, r):
+    L = []
+    R = []
+ 
+    for i in range(p, q + 1):
+        L.append(A[i].get_height())
+    for i in range(q + 1, r + 1):
+        R.append(A[i].get_height())
+    print('L:')
+    print(L)
+    print('R:')
+    print(R)
+
+    i = 0
+    j = 0
+    k = p
+
+    while i < len(L) and j < len(R):
+        if L[i] < R[j]:
+            A[k].set_height(L[i])
+            i = i + 1
+            k = k + 1
+        else:
+            A[k].set_height(R[j])
+            j = j + 1
+            k = k + 1
+
+    while i < len(L):
+        A[k].set_height(L[i])
+        i = i + 1
+        k = k + 1
+
+    while j < len(R):
+        A[k].set_height(R[j])
+        j = j + 1
+        k = k + 1
+
+def merge_sort(A, p, r):
+    if p < r:
+        q = (p + r) / 2
+        merge_sort(A, p, q)
+        merge_sort(A, q + 1, r)
+        merge(A, p, q, r)
+
+def index_gen():
+    merge_sort(rects, 0, samples - 1)
+
+#def save_cnt_gen():
+
+def animate(data):
+    merge_sort(rects, 0, samples - 1)
+    #rects[data].set_color('y')
+
+    return rects
+
+if __name__ == '__main__':
+    global samples, ypos, rects
+
+    if len(sys.argv) < 2:
+        sys.exit('missing operand\nTry \'python visual_merge_sort.py -h\' for more information.')
+
+    outputfile = ''
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hn:o:", ["help", "number=", "ofile="])
+    except getopt.GetoptError:
+        print '''Usage: python visual_merge_sort.py -n <number>
+ or: python visual_merge_sort.py -n <number> -o <outputfile>
+Generate a <number> samples barchart to show how merge sort works. To directly
+see the animation or save it into <outputfile>.gif file.'''
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print '''Usage: python visual_merge_sort.py -n <number>
+ or: python visual_merge_sort.py -n <number> -o <outputfile>
+Generate a <number> samples barchart to show how merge sort works. To directly
+see the animation or save it into <outputfile>.gif file.'''
+            sys.exit()
+        elif opt in ("-n", "--number"):
+            samples = int(arg)
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+
+    fig, ax = plt.subplots()
+    xpos = np.arange(0, samples)
+    datalist = range(1, samples + 1)
+    random.shuffle(datalist)
+    ypos = np.asarray(datalist)
+    rects = ax.bar(xpos, ypos, alpha=0.4, color='b')
+    
+    ani = animation.FuncAnimation(fig, animate, frames=10, repeat=False,
+                                  init_func=init_animate, interval=50)
+    if outputfile == '':
+        plt.show()
+    #else:
+        #ani.save_count = save_cnt_gen()
+        #ani.save(outputfile + '.gif', writer='imagemagick', fps=30, dpi=50)
