7086087b0c87b19d4e7b5d440e8717afaed362f5
heray1990 <heray1990@hotmail.com>
2021-10-06 14:49:40 +0800

    Revert the change in 3fef50811acb57fcceff10fcd9ab4ae0c966c84c. Only display one figure in Merge Sort.

--- src/visual_merge_sort.py
+++ src/visual_merge_sort.py
@@ -185,13 +185,12 @@ see the animation or save it into <outputfile>.gif file.''')
         elif opt in ("-o", "--ofile"):
             outputfile = arg
 
-    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig, ax = plt.subplots()
     xpos = np.arange(0, samples)
     datalist = list(range(1, samples + 1))
     random.shuffle(datalist)
     ypos = np.asarray(datalist)
-    rects = ax1.bar(xpos, ypos, alpha=0.4, color='b')
-    rects2 = ax2.bar(xpos, ypos, alpha=0.4, color='b')
+    rects = ax.bar(xpos, ypos, alpha=0.4, color='b')
     #tmp = []
     #for i in range(samples):
     #    tmp.append(rects[i].get_height())
