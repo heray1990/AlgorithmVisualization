4d697b7fa24f8f2ccf4bd27df4caa1185cb6feea
heray1990 <heray1990@hotmail.com>
2021-10-07 14:00:05 +0800

    1. Python2.x to Python3.x for Quick Sort. 2. Upload code for displaying the correct animation of Quick Sort.

--- src/visual_quick_sort.py
+++ src/visual_quick_sort.py
@@ -18,29 +18,25 @@ def partition(A, p, q):
     x = A[p].get_height()
     i = p
  
+    yield 0, p + 1, i, p, q
     for j in range(p + 1, q + 1):
-        yield 0, j, 0
+        yield 0, j, i, p, q
         if A[j].get_height() <= x:
             i = i + 1
-            yield 1, j, i
-            #tmp = A[j].get_height()
-            #A[j].set_height(A[i].get_height())
-            #A[i].set_height(tmp)
-
-    #A[p].set_height(A[i].get_height())
-    #A[i].set_height(x)
-    yield 2, p, i
-    #return i
+            yield 1, j, i, p, q
+    yield 2, p, i, p, q
 
 def quick_sort(A, p, r):
     if p < r:
-        for i in partition(A, p, r):
-            yield i
-        q = i[2]
-        for j in quick_sort(A, p, q - 1):
-            yield j
-        for k in quick_sort(A, q + 1, r):
-            yield k
+        #print('p = %d, r = %d' % (p, r))
+        for data0 in partition(A, p, r):
+            yield data0
+        q = data0[2]
+        #print('p = %d, q = %d, r = %d' % (p, q, r))
+        for data1 in quick_sort(A, p, q - 1):
+            yield data1
+        for data2 in quick_sort(A, q + 1, r):
+            yield data2
 
 def index_gen():
     datas = quick_sort(rects, 0, samples - 1)
@@ -48,20 +44,38 @@ def index_gen():
         yield data
 
 def animate(data):
-    print data
-    flag, i, j = data
+    # flag = 
+    #   0: scan and compare
+    #   1: exchange
+    #   2: exchange and finish current partition
+    flag, j, i, p, q = data
+    #print(data)
 
-    if not i == j and flag > 0:
-        tmp = rects[i].get_height()
-        rects[i].set_height(rects[j].get_height())
-        rects[j].set_height(tmp)
+    rects[p].set_color('y')
 
     if flag == 0:
-        rects[i].set_color('y')
-        if i > 0:
-            rects[i - 1].set_color('b')
-    if flag == 2:
-        rects[j].set_color('b')
+        if j == p + 1:
+            for idx in range(p, q + 1):
+                rects[idx].set_color('g')
+                rects[p].set_color('y')
+        else:
+            if j > i + 1:
+                rects[j - 1].set_color('r')
+        rects[j].set_color('c')
+    elif flag > 0:
+        if i != j:
+            # Exchange rects[j] and rects[i]
+            rects[j].set_color('r')
+            rects[i].set_color('slateblue')
+            tmp = rects[j].get_height()
+            rects[j].set_height(rects[i].get_height())
+            rects[i].set_height(tmp)
+        else:
+            rects[i].set_color('slateblue')
+
+        if flag == 2:
+            for rect in rects:
+                rect.set_color('b')
 
     return rects
 
@@ -83,18 +97,18 @@ if __name__ == '__main__':
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hn:o:", ["help", "number=", "ofile=", "verbose-debug"])
     except getopt.GetoptError:
-        print '''Usage: python visual_quick_sort.py -n <number>
+        print('''Usage: python visual_quick_sort.py -n <number>
  or: python visual_quick_sort.py -n <number> -o <outputfile>
 Generate a <number> samples barchart to show how quick sort works. To directly
-see the animation or save it into <outputfile>.gif file.'''
+see the animation or save it into <outputfile>.gif file.''')
         sys.exit(2)
 
     for opt, arg in opts:
         if opt in ("-h", "--help"):
-            print '''Usage: python visual_quick_sort.py -n <number>
+            print('''Usage: python visual_quick_sort.py -n <number>
  or: python visual_quick_sort.py -n <number> -o <outputfile>
 Generate a <number> samples barchart to show how quick sort works. To directly
-see the animation or save it into <outputfile>.gif file.'''
+see the animation or save it into <outputfile>.gif file.''')
             sys.exit()
         elif opt in ("-n", "--number"):
             samples = int(arg)
@@ -103,15 +117,15 @@ see the animation or save it into <outputfile>.gif file.'''
 
     fig, ax = plt.subplots()
     xpos = np.arange(0, samples)
-    datalist = range(1, samples + 1)
+    datalist = list(range(1, samples + 1))
     random.shuffle(datalist)
     ypos = np.asarray(datalist)
     rects = ax.bar(xpos, ypos, alpha=0.4, color='b')
-    tmp = []
-    for i in range(0, samples):
-        tmp.append(rects[i].get_height())
-    print tmp
-    
+    #tmp = []
+    #for i in range(0, samples):
+    #    tmp.append(rects[i].get_height())
+    #print(tmp)
+
     ani = animation.FuncAnimation(fig, animate, frames=index_gen, repeat=False,
                                   init_func=init_animate, interval=500)
     if outputfile == '':
